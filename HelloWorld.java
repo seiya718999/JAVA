3266c8423e9f129f7975107ae8cd931a86b7ae04
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2024-01-14 14:39:52 +0000

    [finish]0.2.6

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
         System.out.println("こんにちは");
         System.out.println("31+31");
         System.out.println(31+31);
+        System.out.println(-5 * 2);
+        System.out.println(6 * 6 * 3.14);
     }
 }
