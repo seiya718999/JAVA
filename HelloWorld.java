87abf3eb4b61fe1dc118ebfb06a082b050a7125e
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2024-01-14 14:49:31 +0000

    1.2.4

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,10 @@ public class HelloWorld {
         System.out.println(31+31);
         System.out.println(-5 * 2);
         System.out.println(6 * 6 * 3.14);
+        //変数の準備
         int x;
+        /*変数に
+        代入*/
         x = 6;
         System.out.println(x * x * 3.14);
     }
